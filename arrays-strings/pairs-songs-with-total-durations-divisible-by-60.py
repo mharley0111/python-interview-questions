056bdd97620ab19d1c99aadd719e8d8506aae06c
mharley0111 <michaeldsharley@gmail.com>
2020-11-16 16:55:19 -0500

    added pairs songs with total durations divisible by 60

---
+++ arrays-strings/pairs-songs-with-total-durations-divisible-by-60.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def numPairsDivisibleBy60(time: List[int]) -> int:
+	remainders = collections.defaultdict(int)
+	numPairs = 0
+	for t in time:
+		if t % 60 == 0:
+			numPairs += remainders[0]
+		else:
+			numPairs += remainders[60 - t % 60]
+		remainders[t % 60] += 1
+	return numPairs
