3523ce8bae0b113ad894a413f522fe134ada6958
mharley0111 <michaeldsharley@gmail.com>
2020-11-20 13:00:10 -0500

    updated three sum smaller

--- arrays-strings/3-sum-smaller.py
+++ arrays-strings/3-sum-smaller.py
@@ -1,14 +1,42 @@
 from typing import List
 
+"""
+New Version
+"""
 def threeSumSmaller(nums: List[int], target: int) -> int:
 	nums.sort()
-	count = 0
-	for k in range(len(nums)):
-		i, j = 0, k - 1
-		while i < j:
-			if nums[i] + nums[j] + nums[k] < target:
-				count += j - i
-				i += 1
-			else:
-				j -= 1
-	return count
+	total = 0
+	for i in range(len(nums) - 2):
+		total += twoSumSmaller(nums, i + 1, target - nums[i])
+	
+	return total
+
+
+def twoSumSmaller(nums: List[int], startIndex: int, target: int) -> int:
+	total = 0
+	left = startIndex
+	right = len(nums) - 1
+	while left < right:
+		if nums[left] + nums[right] < target:
+			total += right - left
+			left += 1
+		else:
+			right -= 1
+	
+	return total
+
+"""
+Old Version
+"""
+# def threeSumSmaller(nums: List[int], target: int) -> int:
+# 	nums.sort()
+# 	count = 0
+# 	for k in range(len(nums)):
+# 		i, j = 0, k - 1
+# 		while i < j:
+# 			if nums[i] + nums[j] + nums[k] < target:
+# 				count += j - i
+# 				i += 1
+# 			else:
+# 				j -= 1
+# 	return count
