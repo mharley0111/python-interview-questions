70eaab7e201805529a87c04217441df75bbf5eb6
mharley0111 <michaeldsharley@gmail.com>
2020-11-22 14:28:01 -0500

    added longest common prefix

---
+++ arrays-strings/longest-common-prefix.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def longestCommonPrefix(strs: List[str]) -> str:
+	if strs is None or len(strs) == 0:
+		return ""
+
+	for i in range(len(strs[0])):
+		char = strs[0][i]
+		for j in range(1, len(strs)):
+			if i == len(strs[j]) or strs[j][i] != char:
+				return strs[0][0:i]
+
+		return strs[0]
