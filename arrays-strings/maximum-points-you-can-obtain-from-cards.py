377676849e65a48646ff70c553b6ce40016f2ce3
mharley0111 <michaeldsharley@gmail.com>
2020-12-20 10:24:22 -0500

    added maximum points you can obtain from cards

---
+++ arrays-strings/maximum-points-you-can-obtain-from-cards.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def maxScore(cardPoints: List[int], k: int) -> int:
+  size = len(cardPoints) - k
+  minSubArraySum = float('inf')
+  j = curr = 0
+
+  for i, v in enumerate(cardPoints):
+    curr += v
+    if i - j + 1 > size:
+      curr -= cardPoints[j]
+      j += 1
+    if i - j + 1 == size:
+      minSubArraySum = min(minSubArraySum, curr)
+  
+  return sum(cardPoints) - minSubArraySum
